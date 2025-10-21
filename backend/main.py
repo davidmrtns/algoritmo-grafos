7870ebbd5d5c4834e35607751613991581ad7b5a
davidmrtns <daviddmrtns@gmail.com>
2025-10-21 14:11:20 -0300

    feature: add support to cors

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import spotify
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(spotify.router, prefix="/spotify", tags=["Spotify"])
 
